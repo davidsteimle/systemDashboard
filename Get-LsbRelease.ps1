20ca00d9c19218081f480db788817e0bce4cd961
David Steimle <davidsteimle@gmail.com>
2020-03-13 11:01:13 -0400

    Update Get-LsbRelease.ps1

--- Get-LsbRelease.ps1
+++ Get-LsbRelease.ps1
@@ -9,11 +9,12 @@ class lsb {
 
 $x = lsb_release -a
 
-$z = foreach($y in $x){
+$Result = [lsb]::new()
+
+foreach($y in $x){
     $a = $y.Split(':')
-    $y = @{$($a[0].Replace(" ","")) = $($a[1].Trim())}
-    $y
+    $a[0] = $a[0].Replace(" ","")
+    $Result.$($a[0]) = $($a[1].Trim())
 }
 
-$z
-
+$Result
